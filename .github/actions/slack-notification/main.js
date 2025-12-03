f304cd65b2ccb797621d98af5549d672dc0d6e5e
jocarbon <jorge.carbonell-garcia@capgemini.com>
2025-12-03 14:22:26 +0100

    Añadida Custom Action

---
+++ .github/actions/slack-notification/main.js
@@ -0,0 +1,26 @@
+const core = require('@actions/core')
+const fetch = require('node-fetch');
+
+async function run() {
+    try{
+        const message = core.getInput('message', { required: true})
+        const slackWebhook = core.getInput('slack-webhook', { required: true})
+
+        const response = await fetch(slackWebhook, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(message)
+        });
+
+        if (!response.ok) {
+        throw new Error(`Slack API responded with ${response.status}`);
+        }
+        console.log('Notification sent to Slack successfully!');
+
+    } 
+    catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+run();
